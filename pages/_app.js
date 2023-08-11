56acf153a4b070afa314bb415ebd7e00195ffaa6
vaishnavanm5 <vaishnavanm5@gmail.com>
2023-08-11 21:12:01 +0530

    feat: adding initial setup for code quality and auto commit

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+/* eslint-disable react/prop-types */
+import React from "react"
+import "@/styles/globals.css"
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />
+}
